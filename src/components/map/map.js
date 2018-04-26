7a06d8795cf63d7336fb99a1ac86a71c976468ca
Sandeep Bajaj <sandeepp.bajaj@gmail.com>
2018-04-26 16:10:34 -0700

    Add google-map-react component and use it with SF co-ordinates

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react'
 import './map.css';
 
 class Map extends Component {
+    static defaultProps = {
+        center: { lat: 37.7749, lng: -122.4194 },
+        zoom: 14
+    };
+
     render() {
         return (
             <div className="live-map">
-                Map Component
+                <GoogleMapReact
+                    defaultCenter={ this.props.center }
+                    defaultZoom={ this.props.zoom }>
+                </GoogleMapReact>
             </div>
         );
     }
